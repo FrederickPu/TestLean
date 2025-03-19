882a7bca81c4054bf9e914bbe41cf9b30994c9fc
FrederickPu <pufrederick51@gmail.com>
2025-03-19 12:25:42 -0400

    added lakefile

--- Main.lean
+++ Main.lean
@@ -3,5 +3,5 @@ import TestLean
 def main : IO Unit :=
   IO.println s!"Hello, {hello}!"
 
-theorem womp (a b : Nat) : a + b + c = c + (b + a) := by
+theorem womp1 (a b : Nat) : a + b + c = c + (b + a) := by
   rw [Nat.add_comm, Nat.add_comm a b]
---
+++ lakefile.lean
@@ -0,0 +1,14 @@
+import Lake
+open Lake DSL
+
+package «TestLean» where
+  -- Package version
+  version := v!"0.1.0"
+
+@[default_target]
+lean_lib «TestLean» where
+  -- Library configuration options can be added here
+
+@[default_target]
+lean_exe «testlean» where
+  root := `Mains
